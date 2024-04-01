4f6bb7e5f600b187471373e75352692e2fac7d57
amito <amito@LAPTOP-FS4QCRJK>
2024-03-31 19:47:37 -0500

    2do Ejercicio

--- src/javaclase2/JavaClase2.java
+++ src/javaclase2/JavaClase2.java
@@ -15,6 +15,23 @@ public class JavaClase2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        final float PI = 3.141592f;
+        
+        //Datos del cilindro 1
+        float radioCilindro1 = 4.0f;
+        float alturaCilindro1 = 2.5f;
+        float areaCilindro1;
+        
+        //Datos del cilindro 2
+        float radioCilindro2 = 2.0f;
+        float alturaCilindro2 = 5.0f;
+        float areaCilindro2;
+        
+        areaCilindro1 = 2.0f*PI*(radioCilindro1*alturaCilindro1 + (float)Math.pow((float)radioCilindro1,2.0));
+        areaCilindro2 = 2.0f*PI*(radioCilindro2*alturaCilindro2 + (float)Math.pow((float)radioCilindro2,2.0));
+        
+        System.out.println("El area del cilindro 1 es: "+areaCilindro1+ "metros cuadrados");
+        System.out.println("El area del cilindro 2 es: "+areaCilindro2+ "metros cuadrados");
     }
     
 }
